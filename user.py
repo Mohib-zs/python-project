e81aa12c1fcedee8d6bb7270a5d7a3a06a3639af
Mohib Shaikh <mohibzahid97@gmail.com>
2024-09-20 17:19:15 +0500

    Learned Class UseCases

--- user.py
+++ user.py
@@ -15,8 +15,5 @@ class User:
         print(f"User {self.name} currently work as a {self.job_title} and you can contact them at {self.email}")
 
 
-app_user_1 = User("mohibzahid97@gmail.com", "Mohib Shaikh", "90188d06", "Cloud Intern")
-app_user_1.get_user_info()
-app_user_2 = User("zahidhussain63@gmail.com", "Zahid Shaikh", "90188d06", "HR Analyst")
-app_user_2.get_user_info()
+
 
